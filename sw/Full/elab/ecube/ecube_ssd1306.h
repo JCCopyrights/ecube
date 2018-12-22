9190702528c85c9c627075c4068c918967f62055
JC <jrodrigueztallon@gmail.com>
2018-12-22 01:39:56 +0100

    Added new documentation, HTML unitary test server created

---
+++ sw/Full/elab/ecube/ecube_ssd1306.h
@@ -0,0 +1,12 @@
+
+#ifndef _ECUBE_SSD1306_H_
+#define _ECUBE_SSD1306_H_
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+  
+	void initialize_ssd1306();
+  void test_ssd1306();
+  
+#endif 
